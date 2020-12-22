1652c7b51ff8d5a028fe9b7bc79b4af01ae458f7
Your Name <you@example.com>
2020-12-23 09:56:19 +1100

    Broken home page into smaller components and finished styling menuitem

---
+++ src/pages/homepage.component.jsx
@@ -0,0 +1,11 @@
+import Directory from "../Components/directory/directory.component";
+ import "./homepage.styles.scss";
+
+
+export const HomePage = () => (
+  <div className="homepage">
+    <Directory/>
+  </div>
+);
+
+// export default HomePage;
