754a1fbbac31273abfa2262f3c223159039c15a2
Your Name <you@example.com>
2021-01-02 10:04:20 +1100

    add redux-ersist

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,6 +1,15 @@
 import { combineReducers } from 'redux';
 import userReducer from './user/user.reducer';
 import cartReducer from './cart/cart.reducers';
+import { persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage'; // defaults to localStorage for web
+
+const persistConfig = {
+  key: 'root',
+  storage,
+  whitelist: ['cart'],
+};
 
 const rootReducer = combineReducers({ user: userReducer, cart: cartReducer });
-export default rootReducer;
+
+export default persistReducer(persistConfig, rootReducer);
